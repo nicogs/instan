135d865b71bffa63b66a0194a1d028e7254425e6
morenicogs <nicolas@grootjans.co>
2024-09-25 20:24:30 +0200

    idk if this will fikx it tho

--- service-worker.js
+++ service-worker.js
@@ -1,44 +1,44 @@
-const staticCacheName = "site-static-v5"
-const dynamicCache = "site-dynamic-v4"
+const staticCacheName = "site-static-v7"
+const dynamicCache = "site-dynamic-v6"
 const assets = [
-	"instan/",
-	"instan/index.html",
-	"instan/howto.html",
-	"instan/scripts/libraries/dayjs.min.js",
-	"instan/scripts/libraries/relativeTime.js",
-	"instan/scripts/libraries/uikit-icons.min.js",
-	"instan/scripts/libraries/uikit.min.js",
-	"instan/scripts/libraries/zip-full.min.js",
-	"instan/scripts/createSession.js",
-	"instan/scripts/createTiles.js",
-	"instan/scripts/handleDataIG.js",
-	"instan/scripts/handleSession.js",
-	"instan/scripts/handleUpload.js",
-	"instan/scripts/loadApp.js",
-	"instan/scripts/localStorage.js",
-	"instan/scripts/optionsDropdowns.js",
-	"instan/scripts/userDetailsDOM.js",
-	"instan/scripts/userListDOM.js",
-	"instan/scripts/utilities.js",
-	"instan/styles/uikit.css",
-	"instan/styles/style.css",
+	"/",
+	"/index.html",
+	"/howto.html",
+	"/scripts/libraries/dayjs.min.js",
+	"/scripts/libraries/relativeTime.js",
+	"/scripts/libraries/uikit-icons.min.js",
+	"/scripts/libraries/uikit.min.js",
+	"/scripts/libraries/zip-full.min.js",
+	"/scripts/createSession.js",
+	"/scripts/createTiles.js",
+	"/scripts/handleDataIG.js",
+	"/scripts/handleSession.js",
+	"/scripts/handleUpload.js",
+	"/scripts/loadApp.js",
+	"/scripts/localStorage.js",
+	"/scripts/optionsDropdowns.js",
+	"/scripts/userDetailsDOM.js",
+	"/scripts/userListDOM.js",
+	"/scripts/utilities.js",
+	"/styles/uikit.css",
+	"/styles/style.css",
 	"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css",
 	"https://cdn.jsdelivr.net/npm/chart.js",
-	"instan/img/howto/howto_0.png",
-	"instan/img/howto/howto_1.png",
-	"instan/img/howto/howto_2.png",
-	"instan/img/howto/howto_3.png",
-	"instan/img/howto/howto_4.png",
-	"instan/img/howto/howto_5.png",
-	"instan/img/howto/howto_6.png",
-	"instan/img/howto/howto_7.png",
-	"instan/img/howto/howto_8.png",
-	"instan/img/howto/howto_9.png",
-	"instan/img/howto/howto_10.png",
-	"instan/img/howto/howto_11.png",
-	"instan/img/howto/howto_12.png",
-	"instan/img/icons/launch.png",
-	"instan/img/icons/icon-192x192.png"
+	"/img/howto/howto_0.png",
+	"/img/howto/howto_1.png",
+	"/img/howto/howto_2.png",
+	"/img/howto/howto_3.png",
+	"/img/howto/howto_4.png",
+	"/img/howto/howto_5.png",
+	"/img/howto/howto_6.png",
+	"/img/howto/howto_7.png",
+	"/img/howto/howto_8.png",
+	"/img/howto/howto_9.png",
+	"/img/howto/howto_10.png",
+	"/img/howto/howto_11.png",
+	"/img/howto/howto_12.png",
+	"/img/icons/launch.png",
+	"/img/icons/icon-192x192.png"
 ]
 
 // Install service worker
@@ -82,13 +82,35 @@ self.addEventListener("fetch", event => {
 	)
 })
 
-self.addEventListener("push", (event) => {
-	const promises = []
-    if ('setAppBadge' in self.navigator) {
-		const badgeCount = 69
-		const promise = self.navigator.setAppBadge(badgeCount);
-		promises.push(promise);
-	}
-	promises.push(self.registration.showNotification(event.data.text()))
-	event.waitUntil(Promise.all(promises));
-})
+// self.addEventListener("push", (event) => {
+// 	const promises = []
+//     if ('setAppBadge' in self.navigator) {
+// 		const badgeCount = 69
+// 		const promise = self.navigator.setAppBadge(badgeCount);
+// 		promises.push(promise);
+// 	}
+// 	promises.push(self.registration.showNotification(event.data.text()))
+// 	event.waitUntil(Promise.all(promises));
+// })
+
+// function mimicNotif() {
+// 	const notifLogo = "/img/icons/icon-96x96.png"
+// 	const icon = "Mmic Notif working?"
+
+// 	const options = {
+// 		body: "Okeeey Letsgooooo",
+// 		icon: "/img/icons/icon-192x192.png",
+// 		badge: "/img/icons/icon-192x192.png",
+// 		image: "/img/icons/icon-192x192.png"
+
+// 	}
+
+// 	const promises = []
+//     if ('setAppBadge' in self.navigator) {
+// 		const badgeCount = 1
+// 		const promise = self.navigator.setAppBadge(badgeCount);
+// 		promises.push(promise);
+// 	}
+// 	promises.push(self.registration.showNotification("INSTAN", options))
+// 	Promise.all(promises)
+// }
